2b27928c27e9b4552c65acb7cf9ab8eef03c4df1
Pong Chiva <pongchiva257@gmail.com>
2026-01-31 08:39:42 +0700

    Update  login ft face

--- web-react/vite.config.js
+++ web-react/vite.config.js
@@ -7,11 +7,12 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      }
+  proxy: {
+    '/api': {
+      target: 'http://localhost:8000',
+      changeOrigin: true,
     }
   }
+}
+
 })
